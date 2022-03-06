373508666fd7f29e6357dd94320babc347c1001a
trinhnhathuu <schoolgame2002@gmail.com>
2022-03-06 09:58:26 +0700

    addlayout

--- home.js
+++ home.js
@@ -1,18 +1,56 @@
 var http = require('http');
+var fs = require('fs');
 
-var sever = http.createServer(function(req, res){
-    if(req.url === '/'){
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.end('<h1>Home</h1>');
-    }
-    else if(req.url === '/about'){
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.end('<h1>About</h1>');
-    }
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.write('you have requested: ' + req.url);
-    res.end('Hello World');
-});
+() => {
+    const treeData = [
+        {
+            label: 'Impressionism',
+            value: 'impressionism',
+            children: [
+                {
+                    label: 'Visual Arts',
+                    value: 'visualArts',
+                    children: [
+                        {
+                            label: 'Claude Monet',
+                            value: 'Monet',
+                        },
+                        {
+                            label: 'Pierre-Auguste Renoir',
+                            value: 'Renoir',
+                        },
+                        {
+                            label: 'Édouard Manet',
+                            value: 'Manet',
+                        },
+                    ],
+                },
+                {
+                    label: 'Music',
+                    value: 'music',
+                    children: [
+                        {
+                            label: 'Claude Debussy',
+                            value: 'Debussy',
+                        },
+                        {
+                            label: 'Maurice Ravel',
+                            value: 'Ravel',
+                        }
+                    ]
+                }
+            ],
+        }];
+    return (
+        <Cascader
+            style={{ width: 400 }}
+            treeData={treeData}
+            placeholder="Please select"
+        />
+    );
+};
+
+
 
 sever
     .listen(3000, function(){
